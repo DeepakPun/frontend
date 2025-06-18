f5d4a7b19a99084ed0bfb62f4a53d3c7e1672515
Deepak Pun <pun.deepak1986@gmail.com>
2025-06-18 11:21:18 -0400

    Updated title and refactored Alert to use array and loop

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import Alert from './components/Alert'
 import Blog from './components/Blog'
 import Todo from './components/Todo'
+const messages = [
+  {
+    id: 1,
+    content: 'Hello, 4th July!!',
+  },
+  {
+    id: 2,
+    content: 'LMK what we should do next Sat.',
+  },
+  {
+    id: 3,
+    content: 'How are my Football/Fishing buddies doing?',
+  },
+  {
+    id: 4,
+    content: 'How are my Tennis/Hiking buddies doing?',
+  },
+]
 
 function App() {
   return (
     <div className='p-4'>
-      <Alert message='Hello, 4th July!!' />
-      <Alert message='LMK what we should do next Sat?' />
-      <Alert message='How are my Football/Fishing buddies doin?' />
-      <Alert message='How are Tennis/Hiking buddies doin?' />
+      {messages.map(message => (
+        <Alert key={message.id} message={message.content} />
+      ))}
       <Todo />
       <Blog />
       <h1>Hello how are you?</h1>
