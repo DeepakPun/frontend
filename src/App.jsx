825f6c96bc4f207d680c11b0197c9cd985b81bce
Deepak Pun <pun.deepak1986@gmail.com>
2025-06-18 12:03:00 -0400

    Removed message and added image

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,36 @@
 import Alert from './components/Alert'
 import Blog from './components/Blog'
+import Pic from './components/Image'
 import Todo from './components/Todo'
 const messages = [
-  {
-    id: 1,
-    content: 'Hello, 4th July!!',
-  },
-  {
-    id: 2,
-    content: 'LMK what we should do next Sat.',
-  },
-  {
-    id: 3,
-    content: 'How are my Football/Fishing buddies doing?',
-  },
-  {
-    id: 4,
-    content: 'How are my Tennis/Hiking buddies doing?',
-  },
+  // {
+  //   id: 1,
+  //   content: 'Hello, 4th July!!',
+  // },
+  // {
+  //   id: 2,
+  //   content: 'LMK what we should do next Sat.',
+  // },
+  // {
+  //   id: 3,
+  //   content: 'How are my Football/Fishing buddies doing?',
+  // },
+  // {
+  //   id: 4,
+  //   content: 'How are my Tennis/Hiking buddies doing?',
+  // },
 ]
 
 function App() {
   return (
     <div className='p-4'>
-      {messages.map(message => (
-        <Alert key={message.id} message={message.content} />
-      ))}
+      {messages &&
+        messages.length > 0 &&
+        messages.map(message => (
+          <Alert key={message.id} message={message.content} />
+        ))}
       <Todo />
+      <Pic />
       <Blog />
       <h1>Hello how are you?</h1>
     </div>
---
+++ src/components/Image.jsx
@@ -0,0 +1,11 @@
+import lfc from '/images/LFC.jpeg'
+
+const Pic = () => {
+  return (
+    <div>
+      <img src={lfc} alt='LFC' width={350} height={350} />
+      <p>LFC</p>
+    </div>
+  )
+}
+export default Pic
